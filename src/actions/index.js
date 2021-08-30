174cd34f4bc3f16dad723a3e04b6a2c42cfe79c5
jinmc <jinmo55555@gmail.com>
2021-08-30 15:07:42 +0900

    add dependencies and middleware also

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,4 @@
 import axios from 'axios';
 
+const API_KEY = '8bbca77db946c20b6eb3f38ea3de2f5c';
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
 import App from './components/App';
+import reducers from './reducers';
+
+const createStoreWidthMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWidthMiddleware}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
